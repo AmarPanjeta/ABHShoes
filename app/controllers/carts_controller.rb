58e6884693836b1cf34a86a0cb6b6f671e0ff9d8
AmarPanjeta <apanjeta1@etf.unsa.ba>
2016-02-26 11:18:56 +0100

    cart fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
+  include CurrentCart
   before_action :authorize, only: [:index]
+  before_action :set_cart, only: [:index]
   def index
     @price=0;
     @quantity=current_user.cart.shoes.count
